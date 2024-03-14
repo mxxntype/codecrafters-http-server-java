ab2aa13a7e98acbceb56f7a4af06eb69510280e4
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-03-14 21:22:39 +0300

    refactor: Create `ClientHandler` class, infer some types, add `final`s

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,24 +7,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
     public static void main(final String[] args) {
         try {
-            final ServerSocket server = new ServerSocket(4221);
+            final var server = new ServerSocket(4221);
             server.setReuseAddress(true);
             log(LogLevel.INFO, "Server started on port " + server.getLocalPort());
 
             while (true) {
-                final Socket client = server.accept();
+                final var client = server.accept();
                 log(LogLevel.INFO, "New connection on port " + client.getPort());
                 new ClientHandler(client).start();
             }
@@ -34,59 +24,54 @@ public class Main {
     }
 
     private static class ClientHandler extends Thread {
-        private final Socket client;
+        private final Socket clientSocket;
 
-        public ClientHandler(Socket client) {
-            this.client = client;
+        public ClientHandler(final Socket clientSocket) {
+            this.clientSocket = clientSocket;
         }
 
         @Override
         public void run() {
             try {
-                handleClient(client);
-            } catch (IOException e) {
+                handleClient(this.clientSocket);
+                this.clientSocket.close();
+            } catch (final IOException e) {
                 log(LogLevel.ERROR, "Error handling client: " + e.getMessage());
-            } finally {
-                try {
-                    client.close();
-                } catch (IOException e) {
-                    log(LogLevel.ERROR, "Error closing client connection: " + e.getMessage());
-                }
             }
         }
 
         /**
-         * @param client
+         * @param clientSocket
          * @throws IOException
          */
-        private static void handleClient(final Socket client) throws IOException {
-            final BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(client.getInputStream()));
+        private static void handleClient(final Socket clientSocket) throws IOException {
+            final var reader =
+                    new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             String request = null;
             while ((request = reader.readLine()) != null && !request.isBlank()) {
                 log(LogLevel.DEBUG, "Read: " + request);
-                final String[] tokens = request.split(" ");
-                final String requestType = tokens[0].trim();
+                final var tokens = request.split(" ");
+                final var requestType = tokens[0].trim();
                 if (requestType.equals("GET")) {
-                    String path = tokens[1];
-                    if (path.equals("/")) {
-                        writeResponse(client, "HTTP/1.1 200 OK\r\n\r\n");
-                    } else {
+                    final var path = tokens[1];
+                    if (path.equals("/")) writeResponse(clientSocket, "HTTP/1.1 200 OK\r\n\r\n");
+                    else {
                         log(LogLevel.WARN, "Path " + path + " is invalid");
-                        writeResponse(client, "HTTP/1.1 404 Not Found\r\n\r\n");
+                        writeResponse(clientSocket, "HTTP/1.1 404 Not Found\r\n\r\n");
                     }
                 }
             }
         }
 
         /**
-         * @param client
+         * @param clientSocket
          * @param response
          * @throws IOException
          */
-        private static void writeResponse(final Socket client, String response) throws IOException {
-            final BufferedWriter writer =
-                    new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        private static void writeResponse(final Socket clientSocket, final String response)
+                throws IOException {
+            final var writer =
+                    new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
             writer.write(response);
             writer.flush();
             log(LogLevel.DEBUG, "Wrote: " + response.trim());
@@ -100,6 +85,16 @@ public class Main {
         WARN,
         ERROR,
     }
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
 
     /**
      * @param logLevel
