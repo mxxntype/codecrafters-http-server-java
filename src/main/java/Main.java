6ac1616fc8e92c0d453d4010a75e7e98ef8caeab
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-03-14 15:05:53 +0300

    refactor: Colorize output, extract reading & writing to methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,31 +7,55 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void main(final String[] args) {
         try {
             // Start a server.
-            ServerSocket server = new ServerSocket(4221);
+            final ServerSocket server = new ServerSocket(4221);
             server.setReuseAddress(true);
-            Socket client = server.accept();
-            System.err.println("Got new connection on port " + client.getPort());
+            final Socket client = server.accept();
+            System.err.println(ANSI_CYAN + "[NEW CONNECTION]: " + ANSI_RESET + client.getPort());
 
-            // Read input.
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(client.getInputStream()));
-            String in = reader.readLine();
-            System.err.println("Received input: " + in);
+            readRequest(client);
+            respondToClient(client);
 
-            // Respond with HTTP OK (200)
-            BufferedWriter writer =
-                    new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
-            writer.write("HTTP/1.1 200 OK\r\n\r\n");
-            writer.flush();
-            System.err.println("Wrote HTTP OK response (200)");
-
-            // Stop the server.
             server.close();
-        } catch (IOException e) {
-            System.err.println("IOException: " + e.getMessage());
+        } catch (final IOException e) {
+            System.err.println(ANSI_RED + "[EXCEPTION]: " + ANSI_RESET + e.getMessage());
         }
+    }
+
+    /**
+     * @param client
+     * @throws IOException
+     */
+    private static void readRequest(final Socket client) throws IOException {
+        final BufferedReader reader =
+                new BufferedReader(new InputStreamReader(client.getInputStream()));
+        String request = null;
+        while ((request = reader.readLine()) != null && !request.isBlank()) {
+            System.err.println(ANSI_PURPLE + "[INPUT]: " + ANSI_RESET + request);
+        }
+    }
+
+    /**
+     * @param client
+     * @throws IOException
+     */
+    private static void respondToClient(final Socket client) throws IOException {
+        final BufferedWriter writer =
+                new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        writer.write("HTTP/1.1 200 OK\r\n\r\n");
+        writer.flush();
+        System.err.println(ANSI_CYAN + "[RESPONSE]: " + ANSI_RESET + "200 OK");
     }
 }
