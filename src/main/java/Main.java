74a190a721a8eb2314278670eef02ad2e43c0601
mxxntype <59417007+mxxntype@users.noreply.github.com>
2024-03-14 12:39:03 +0300

    feat: Accept TCP connections on port `4221`

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
 
-    // Uncomment this block to pass the first stage
-    //
-    // ServerSocket serverSocket = null;
-    // Socket clientSocket = null;
-    //
-    // try {
-    //   serverSocket = new ServerSocket(4221);
-    //   serverSocket.setReuseAddress(true);
-    //   clientSocket = serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
-  }
+        try {
+            serverSocket = new ServerSocket(4221);
+            serverSocket.setReuseAddress(true);
+            Socket clientSocket = serverSocket.accept();
+            System.err.println("Got new connection on port " + clientSocket.getPort());
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+        }
+    }
 }
